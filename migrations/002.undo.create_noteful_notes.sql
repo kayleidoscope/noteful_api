4ff2299805e2159fa87f392a3211ef5eacf7e9e2
Kay Holten <holtenkay@gmail.com>
2020-12-17 14:36:19 -0800

    creates working noteful api

---
+++ migrations/002.undo.create_noteful_notes.sql
@@ -0,0 +1,4 @@
+ALTER TABLE noteful_notes
+    DROP COLUMN folder;
+
+DROP TABLE IF EXISTS noteful_notes;
--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const { restart } = require('nodemon')
+const notesRouter = require('./notes/notes-router')
+const foldersRouter = require('./folders/folders-router')
 
 const app = express()
 
@@ -15,6 +17,9 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use('/api/notes', notesRouter)
+app.use('/api/folders', foldersRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
